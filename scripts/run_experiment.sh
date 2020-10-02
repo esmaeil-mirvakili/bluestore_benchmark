1d47520b58319534008a702a6e6a32615a81a202
Esmaeil <smirvaki@ucsc.edu>
2020-10-02 00:15:22 -0700

    fix bugs

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -159,7 +159,7 @@ function prec() {
 	done
 
 	printf "Done!\n"
-	sudo LD_LIBRARY_PATH="$CEPH_HOME"/build/lib:$LD_LIBRARY_PATH "$FIO_HOME"/fio prec.fio
+	LD_LIBRARY_PATH="$CEPH_HOME"/build/lib:$LD_LIBRARY_PATH "$FIO_HOME"/fio prec.fio
 }
 
 
