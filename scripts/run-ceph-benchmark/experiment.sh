e69feae1f398364c884fa839a8a008bf0a594312
Esmaeil <smirvaki@ucsc.edu>
2020-10-25 00:30:08 -0700

    add output tp automation

--- scripts/run-ceph-benchmark/experiment.sh
+++ scripts/run-ceph-benchmark/experiment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+output_path=$1
 io_depth_settings=(16 32 48)
 rw_settings=(randwrite randread randrw)
 bs_settings=(1024 4096 102400)
+path_del="/"
+prefix="${output_path}/results"
 for io_depth in "${io_depth_settings[@]}"
 do
 	for rw in "${rw_settings[@]}"
@@ -9,6 +12,11 @@ do
 		for bs in "${bs_settings[@]}"
 		do
 			sudo ./run-fio-queueing-delay.sh $io_depth $rw $bs
+			path=$prefix$path_del$io_depth$path_del$rw$path_del$bs
+			echo $path
+			sudo mkdir -p "$path"
+			sudo mv codel_* $path
+			sudo mv dump-fio-bench-* $path
 		done
 	done
 done
