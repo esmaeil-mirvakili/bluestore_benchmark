c8a01bf941201d1ebed1d57a4db3227fb3ab7179
Esmaeil <smirvaki@ucsc.edu>
2020-11-07 20:28:18 -0800

    add more params

--- scripts/run-ceph-benchmark/experiment.sh
+++ scripts/run-ceph-benchmark/experiment.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 output_path=$1
-io_depth_settings=(16 32 48)
-rw_settings=(randwrite randread randrw)
-bs_settings=(1024 4096 102400)
+codel_activated=0
+io_depth_settings=(16 48 512)
+rw_settings=(randwrite)
+bs_settings=(4096 131072)
+declare -A target_lats=( [16]=0 [48]=0 [512]=0)
+declare -A intervals=( [16]=0 [48]=0 [512]=0)
 path_del="/"
 prefix="${output_path}/results"
 post_fix="_fio.json"
+
 for io_depth in "${io_depth_settings[@]}"
 do
 	for rw in "${rw_settings[@]}"
 	do
 		for bs in "${bs_settings[@]}"
 		do
-			sudo ./run-fio-queueing-delay.sh $io_depth $rw $bs
+			target_lat=${target_lats[$io_depth]}
+			interval=${intervals[$io_depth]}
+			init_batch_size=${io_depth}
+			sudo ./run-fio-queueing-delay.sh $io_depth $rw $bs $codel_activated $target_lat $interval $init_batch_size
 			path=$prefix$path_del$io_depth$path_del$rw$path_del$bs
 			echo $path
 			sudo mkdir -p "$path"
--- scripts/run-ceph-benchmark/start_ceph.sh
+++ scripts/run-ceph-benchmark/start_ceph.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+codel=$1
+if [ "$codel" = 1 ] ; then
+    codel=true
+fi
+target_lat=$2
+interval=$3
+batch_size=$4
+batch_size_limit_ratio=1.5
+adaptive_down_sizing=true
+
 #sudo bin/ceph osd pool delete mybench mybench --yes-i-really-really-mean-it
 #sudo ../src/stop.sh
 
@@ -7,7 +17,7 @@
 #sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -b -n -x -l --without-dashboard
 #sudo MON=1 OSD=2 MDS=0 ../src/vstart.sh -n -b -x -l --without-dashboard
 #sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -n -b -x -l  -o 'bluestore fsck on mkfs = false' -o 'bluestore fsck on mount = false' -o 'bluestore fsck on umount = false' -o 'bluestore block db path = ' -o 'bluestore block wal path = ' -o 'bluestore block wal create = false' -o 'bluestore block db create = false' --without-dashboard
-sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -n -o 'bluestore block = /dev/sdc' -o 'bluestore block path = /dev/sdc' -o 'bluestore fsck on mkfs = false' -o 'bluestore fsck on mount = false' -o 'bluestore fsck on umount = false' -o 'bluestore block db path = ' -o 'bluestore block wal path = ' -o 'bluestore block wal create = false' -o 'bluestore block db create = false' --without-dashboard
+sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -n -o 'bluestore codel = "$codel"' -o 'bluestore codel target latency = $target_lat' -o 'bluestore codel interval = $interval' -o 'bluestore codel init batch size = $batch_size' -o 'bluestore codel batch size limit ratio = $batch_size_limit_ratio' -o 'bluestore codel adaptive down sizing = $adaptive_down_sizing' -o 'bluestore block = /dev/sdc' -o 'bluestore block path = /dev/sdc' -o 'bluestore fsck on mkfs = false' -o 'bluestore fsck on mount = false' -o 'bluestore fsck on umount = false' -o 'bluestore block db path = ' -o 'bluestore block wal path = ' -o 'bluestore block wal create = false' -o 'bluestore block db create = false' --without-dashboard
 #sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -n -o 'bluestore block = /dev/sdc' -o 'bluestore block path = /dev/sdc'  --without-dashboard
 #sudo MON=1 OSD=1 MDS=0 ../src/vstart.sh -n -o 'bluestore block = /dev/sdc' -o 'bluestore block path = /dev/sdc'
 #sudo bin/ceph osd pool create mybench 128 128
