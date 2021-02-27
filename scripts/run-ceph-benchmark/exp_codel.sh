b0fa23dbf6a58b6b27fbe6391c726948e9b6171d
esmaeil-mirvakili <smirvaki@ucsc.edu>
2021-02-26 20:30:38 -0800

    add codel exp

--- scripts/run-ceph-benchmark/exp_codel.sh
+++ scripts/run-ceph-benchmark/exp_codel.sh
@@ -5,13 +5,13 @@ drive="/dev/sdc"
 io_depth_settings=(40960)
 rw_settings=(randwrite)
 bs_settings=(4)  # KB
-target_lats=(0)
-intervals=(0)
+target_lats=(8 12)
+intervals=(50 100 150 200 250 300 350 400 450 500 550 600 650 700)
 path_del="/"
 del="_"
 prefix="${output_path}/results"
-post_fix="_fio.json"
-
+sudo rm -f codel_*
+sudo rm -f dump-fio-bench-*
 for io_depth in "${io_depth_settings[@]}"
 do
         for rw in "${rw_settings[@]}"
@@ -23,14 +23,22 @@ do
                                 for target_lat in "${target_lats[@]}"
                                 do
                                         bs_bytes=$(( bs*1024 ))
+                                        target_lat_ns=$(( target_lat*1000000 ))
+                                        interval_ns=$(( interval*1000000 ))
                                         init_batch_size=40960
+                                        echo "1"> codel.settings
+                                        echo "$target_lat_ns">> codel.settings
+                                        echo "$interval_ns">> codel.settings
+                                        echo "512000">> codel.settings
+                                        echo "51200">> codel.settings
+                                        echo "50">> codel.settings
                                         sudo ./run-fio-queueing-delay.sh $io_depth $rw $bs_bytes $codel_activated $target_lat $interval $init_batch_size $drive
-                                        path=$prefix$path_del$interval$del$target_lat$path_del$rw$path_del$bs
+                                        path=$prefix$path_del$target_lat$del$interval$path_del$rw$path_del$bs
                                         echo $path
                                         sudo mkdir -p "$path"
-                                        name=$(find . -type f -name "codel_log*"  | cut -c3-)
                                         sudo mv codel_* $path
-                                        sudo mv dump-fio-bench-* $path$path_del$name$post_fix
+                                        name="fio_${target_lat}_${interval}.json"
+                                        sudo mv dump-fio-bench-* $path$path_del$name
                                 done
                         done
                 done
